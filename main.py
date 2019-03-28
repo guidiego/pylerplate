79e1981d1ee1400a336624142e51de6a52df17f7
Pedro Daumas <pedrodaumas@gmail.com>
2019-03-28 20:01:57 -0300

    Add: User model

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from flask import Flask
-from flask_apispec import use_kwargs, marshal_with
+from flask_apispec import use_kwargs, marshal_with, FlaskApiSpec
+from flask_bcrypt import Bcrypt
 
 from marshmallow import fields, Schema
 
 from modules.user.models import User
 
 app = Flask(__name__)
+docs = FlaskApiSpec(app)
+bcrypt = Bcrypt(app)
 
 class UserSchema(Schema):
     class Meta:
-        fields = ('id', 'created_at', 'updated_at', 'deleted_at')
+        fields = ('id', 'email', 'password', 'created_at', 'updated_at', 'deleted_at')
     
 @app.route('/pets')
-@use_kwargs({'category': fields.Str(), 'size': fields.Str()})
 @marshal_with(UserSchema(many=True))
 def get_pets(**kwargs):
     return User.query.filter_by(**kwargs)
+
+docs.register(get_pets)
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host='0.0.0.0') 
--- modules/user/models.py
+++ modules/user/models.py
@@ -1,4 +1,38 @@
+import datetime
+import random
+import string
+
+from passlib import hash
+
 from database import Base
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
+from sqlalchemy import Column, DateTime, Integer, Text, String, Binary, text
+
+from main import bcrypt
 
 class User(Base):
-    pass
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(Binary(60), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now, server_default=text("NOW()"))
+    updated_at = Column(
+        DateTime,
+        onupdate=datetime.datetime.now, nullable=True, server_default=text("NULL ON UPDATE NOW()")
+    )
+    deleted_at = Column(DateTime)
+
+    def __init__(self, *args, **kwargs):
+        self.email = kwargs['email']
+        self._password = kwargs['password']
+
+    @hybrid_property
+    def _password(self):
+        return self.password
+
+    @_password.setter
+    def set_password(self, plaintext_password):
+        self.password = bcrypt.generate_password_hash(plaintext_password)
