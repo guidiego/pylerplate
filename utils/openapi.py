9786b6e35633e52873d39bd199fdfa0be3454101
Guilherme Diego <guidiego.expgames@gmail.com>
2019-03-28 23:11:30 -0300

    Add: Flakes and PEP8

--- utils/openapi.py
+++ utils/openapi.py
@@ -1,23 +1,31 @@
-from flask_apispec import marshal_with as mw, use_kwargs as kw, doc, MethodResource
+from flask_apispec import marshal_with as mw, use_kwargs as kw, doc
 
-def api(path='/', methods=['GET'], marshal_with=None, use_kwargs=None, **wrapkwargs):
+
+def api(
+    path='/',
+    methods=['GET'],
+    marshal_with=None,
+    use_kwargs=None,
+    **wrapkwargs,
+):
     def decorator(func):
         new_func = func
-        
+
         if marshal_with is not None:
             new_func = mw(marshal_with)(new_func)
 
         if use_kwargs is not None:
             new_func = kw(use_kwargs)(new_func)
 
-        if wrapkwargs :
-            new_func = doc(**wrapkwargs)(new_func) 
-        
+        if wrapkwargs:
+            new_func = doc(**wrapkwargs)(new_func)
+
         new_func.__url__ = path
         new_func.__method__ = methods
 
         return new_func
     return decorator
+
 
 def register_rules(app, resources):
     for resource in resources:
