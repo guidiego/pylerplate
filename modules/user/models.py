d0f54e695fb933cae06f00a1f9050becd46a5beb
Guilherme Diego <guidiego.expgames@gmail.com>
2019-03-31 12:20:07 -0300

    Refactor: Permission and fix relationship

--- modules/user/models.py
+++ modules/user/models.py
@@ -1,8 +1,9 @@
-from database import db
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import enum
 
-from app import bcrypt
+from database import db
+from sqlalchemy import Column, String, Integer, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+from database import db
 
 
 class User(db.Model):
@@ -10,11 +11,27 @@ class User(db.Model):
 
     email = Column(String, unique=True, nullable=False)
     password = Column(String(60), nullable=False)
-
-    # TODO: Make it works
-    permissions = relationship('modules.permission.models.UserPermission', backref='user')
+    permissions = relationship('UserPermission', backref='user')
 
     # TODO: Improve the way to do that
     # @password.setter
     # def setpassword(self, plaintext_password):
     #     self.password = bcrypt.generate_password_hash(plaintext_password)
+
+
+class PermissionEnum(enum.Enum):
+    ADMIN = 0
+    COMMON = 1
+
+class UserPermission(db.Model):
+    __tablename__ = 'user_group'
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    permission = Column(Enum(PermissionEnum), nullable=False)
+
+    @classmethod
+    def set_common(cls, user_id):
+        return cls(
+            user_id=user_id,
+            permission=PermissionEnum.COMMON
+        )
