913c083287c693183e258e6e8ba347b086e21a5e
Pedro Daumas <pedrodaumas@gmail.com>
2019-04-02 00:59:31 -0300

    Fix: User permission on swagger view

--- modules/user/schemas.py
+++ modules/user/schemas.py
@@ -1,9 +1,25 @@
 from utils.marshmallow import BaseSchema
-from .models import User
+from marshmallow import fields
+from marshmallow_enum import EnumField
+from .models import User, UserPermission, PermissionEnum
 
 class UserSchema(BaseSchema):
-    dump_only = ['created_at', 'updated_at', 'deleted_at', 'id', 'permissions']
+    dump_only = ['created_at', 'updated_at', 'deleted_at', 'id']
     load_only = ['password']
+    permissions = fields.Nested('UserPermissionSchema', exclude=['permissions'], many=True)
 
     class Meta:
         model = User
+
+class UserPermissionSchema(BaseSchema):
+    # permission = EnumField(PermissionEnum, by_value=True)
+    # permission = fields.Method("convert_enum")
+    permission = fields.Str(dump_only=True)
+
+    # def convert_enum(self, obj):
+    #     obj = obj.permission.name
+    #     print(obj.permission.name)
+    #     return obj
+
+    class Meta:
+        model = UserPermission
