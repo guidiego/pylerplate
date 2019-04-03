f2cf7cabbd19c08faf6d0336013684e705aed8ae
Guilherme Diego <guidiego.expgames@gmail.com>
2019-04-03 00:22:50 -0300

    Change APISpec data, add redoc and add create module

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ from database import db
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.__docs__ = FlaskApiSpec(app)
 app.config.from_object('config.Config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config.update({
+    'APISPEC_TITLE': 'Pylerplate',
+    'APISPEC_VERSION': 'Pylerplate',
+    'APISPEC_SWAGGER_URL': '/swagger/',
+    'APISPEC_SWAGGER_UI_URL': None,
+})
+
+app.__docs__ = FlaskApiSpec(app)
 marshmallow = Marshmallow(app)
 bcrypt = Bcrypt(app)
 db.init_app(app)
